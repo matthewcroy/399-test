8407dc912e0d8c155373018bd1ae05e9aac35a10
matthewcroy <72712335+matthewcroy@users.noreply.github.com>
2022-09-30 17:28:00 +1300

    Add files via upload

--- mcro427.js
+++ mcro427.js
@@ -1,119 +1,30 @@
-function Version() {
-    const fp = fetch('https://cws.auckland.ac.nz/gas/api/Version');
-    const sp = fp.then((response) => response.text());
-    sp.then((data) => {
-        document.getElementById('version').innerText = data;
-    });
-}
+let saveFile = () => {
+    // Get the data from each element on the form.
+    const name = document.getElementById("txtName");
+    const age = document.getElementById("txtAge");
+    const email = document.getElementById("txtEmail");
+    const country = document.getElementById("selCountry");
+    const msg = document.getElementById("msg");
 
-const home = () => {
-    document.getElementById("home").style.display = "block";
-    document.getElementById("shop").style.display = "none";
-    document.getElementById("game").style.display = "none";
-    document.getElementById("comment").style.display = "none";
-    document.getElementById("register").style.display = "none";
-    document.getElementById("login").style.display = "none";
-}
+    // This variable stores all the data.
+    let data = "\r Name: " + name.value + " \r\n " + "Age: " + age.value + " \r\n " + "Email: " + email.value + " \r\n " + "Country: " + country.value + " \r\n " + "Message: " + msg.value;
+    console.log(data); //printing form data into the console
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([data], { type: "text/plain" });
+    var filename = "test"
 
-const shop = () => {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("shop").style.display = "block";
-    document.getElementById("game").style.display = "none";
-    document.getElementById("comment").style.display = "none";
-    document.getElementById("register").style.display = "none";
-    document.getElementById("login").style.display = "none";
-    getProducts();
-}
+    const sFileName = filename; // The file to save the data.
 
-const game = () => {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("shop").style.display = "none";
-    document.getElementById("game").style.display = "block";
-    document.getElementById("comment").style.display = "none";
-    document.getElementById("register").style.display = "none";
-    document.getElementById("login").style.display = "none";
-}
+    let newLink = document.createElement("a");
+    newLink.download = "test";
 
-const comment = () => {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("shop").style.display = "none";
-    document.getElementById("game").style.display = "none";
-    document.getElementById("comment").style.display = "block";
-    document.getElementById("register").style.display = "none";
-    document.getElementById("login").style.display = "none";
-    document.getElementById("myRecentComments").innerHTML = '<iframe src="https://cws.auckland.ac.nz/gas/api/Comments"></iframe>';
-}
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    } else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
 
-const register = () => {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("shop").style.display = "none";
-    document.getElementById("game").style.display = "none";
-    document.getElementById("comment").style.display = "none";
-    document.getElementById("register").style.display = "block";
-    document.getElementById("login").style.display = "none";
-}
-
-const login = () => {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("shop").style.display = "none";
-    document.getElementById("game").style.display = "none";
-    document.getElementById("comment").style.display = "none";
-    document.getElementById("register").style.display = "none";
-    document.getElementById("login").style.display = "block";
-}
-
-function registerUser() {
-    let myStr = JSON.stringify({
-        "username": `${document.getElementById("username").value}`,
-        "password": `${document.getElementById("password").value}`,
-        "address": `${document.getElementById("address").value}`
-    })
-    const fp = fetch('https://cws.auckland.ac.nz/gas/api/Register', {
-        method: 'POST',
-        body: myStr,
-        headers: { "Content-Type": "application/json" }
-    });
-    const sp = fp.then((response) => response.text());
-    sp.then((response) => {
-        document.getElementById("registrationMessage").innerText = response;
-    });
-
-}
-
-function loginUser() {
-    const fp = fetch('https://cws.auckland.ac.nz/gas/api/VersionA', {
-        headers: {
-            "Content-Type": "text/plain",
-            'Authorization': 'Basic ' + btoa(`${document.getElementById("usernameLog").value}:${document.getElementById("passwordLog").value}`),
-        }
-    });
-    fp.then((response) => {
-        if (response.status === 200) {
-            var logoutbutton = document.getElementById("logoutbutton");
-            logoutbutton.style.display = "inline";
-            logoutbutton.innerHTML = `Logout (${document.getElementById("usernameLog").value})`;
-            document.getElementById("loginbutton").style.display = "none";
-            loggedIn = true;
-            home();
-
-        } else {
-            alert("Incorrect Login Details. Try Again.");
-            return;
-        }
-    });
-}
-
-function logout() {
-    var logoutbutton = document.getElementById("logoutbutton");
-    logoutbutton.style.display = "none";
-    document.getElementById("loginbutton").style.display = "inline";
-    loggedIn = false;
-    home();
-}
-
-
-window.onload = home();
-Version();
-let loggedIn = false;
-let gameId = "";
-
+    newLink.click();
+};
